6e1562aec623abd8cbda3c02540b7baeb8bea41d
BolatDanial <bolatdanial0103@gmail.com>
2025-02-11 11:09:47 +0500

    Added main reservation pages to react app

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import SeatSelection from './SeatSelection';
+import Paying from './Paying';
+import Paid from './Paid';
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/booking" element={<SeatSelection />} />
+        <Route path="/paying" element={<Paying />} />
+        <Route path="/paid" element={<Paid />} />
       </Routes>
     </Router>
   );
